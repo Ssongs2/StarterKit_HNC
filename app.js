63ceb89947605c2770357c335e0a5d6ec7d458e9
SongEunJi <sejeq1995@gmail.com>
2021-07-30 17:37:16 +0900

    ch_03 최적화

--- app.js
+++ app.js
@@ -1,42 +1,64 @@
 // let ajax = new XMLHttpRequest(); // 변수
-
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest(); //상수
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json"
 
-ajax.open('GET', NEWS_URL, false); // 동기적으로 처리하겠다.
-ajax.send();
+function getData(url){
 
-// JSON -> Object 
-
-const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement('ul');
-
-window.addEventListener('hashchange', function(){
-    console.log ('해시가 변경 됨.');
-    console.log(location.hash);
-    const id = location.hash.substring(1);
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    ajax.open('GET', url, false); // 동기적으로 처리하겠다.
     ajax.send();
 
-    const newsContent = JSON.parse(ajax.response);
-    console.log(newsContent)
+    return JSON.parse(ajax.response);
+}
+
+// JSON -> Object  
+const newsFeed = getData(NEWS_URL);
+const ul = document.createElement('ul');
+
+// 해시 내용이 변경될 때 발생
+window.addEventListener('hashchange', function(){
+    //console.log ('해시가 변경 됨.');
+    //console.log(location.hash);
+    // location
+    const id = location.hash.substring(1);
+
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+    const title = document.createElement('h1');
+
+    title.innerHTML = newsContent.title;
+    newsContent.title;
+    
+    content.appendChild(title);
+    console.log(newsContent);
 })
 
 for (let i = 0; i < 10; i++) {
+    const div = document.createElement('div');
     const li = document.createElement('li');
     const a = document.createElement('a');
 
-    a.href = `#${newsFeed[i].id}`;
-    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count })`;
+    div.innerHTML = 
+    `
+    <li><a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count }</a></li>
+    `;
 
     //a.addEventListener('click', function(){});
 
-    li.appendChild(a)
-    ul.appendChild(li);
+    // a.href = `#${newsFeed[i].id}`;
+    // a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count })`;
+    //li.appendChild(a)
+
+    //ul.appendChild(div.children[0]);
+    ul.appendChild(div.firstElementChild);
 }
 
 // 백틱 : 문자열로 만들 수 있음
 // 브라킷 대괄호
 // 브레이스 {} 중괄호
-document.getElementById('root').appendChild(ul);
+//document.getElementById('root').appendChild(ul);
+//document.getElementById('root').appendChild(content);
+
+container.appendChild(ul);
+container.appendChild(content);
